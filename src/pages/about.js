dba0fb626586885681cdd4875eedc6a5f56ccb20
sepmein <sepmein@gmail.com>
2021-06-16 21:11:29 +0200

    finished gatsby part2=

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>
+                About me
+            </title>
+            <Link to="/">Back to Home</Link>
+            <h1>
+                About me
+            </h1>
+
+        </main>
+    )
+}
+
+export default AboutPage
