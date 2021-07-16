1b25f163d095e9dd0f50b17993d6ee4118533832
sepmein <sepmein@gmail.com>
2021-07-16 14:42:12 +0200

    updated data page

--- src/pages/data.js
+++ src/pages/data.js
@@ -1,14 +1,26 @@
 import * as React from "react";
 import Frame from "../components/layout";
 import { Typography, Divider } from "antd";
-const { Title } = Typography;
+const { Title, Paragraph } = Typography;
 
 class Data extends React.Component {
   render() {
     return (
-      <Frame pageTitle='Data'>
+      <Frame pageTitle="Data">
         <Typography>
           <Title>Add more data to This Project</Title>
+          <Paragraph>
+            MalariaOne has a full-featured backend to manage the project
+            database. The backend allows user enter, export data and manipulate
+            database structure.
+          </Paragraph>
+          <Paragraph>
+            Currently, only invited users are allowed to enter the system.
+          </Paragraph>
+          <Paragraph>
+            The system could be access at:{" "}
+            <a href="http://malaria.one:8055">http://malaria.one:8055</a>
+          </Paragraph>
           <Title level={2} id="db">
             The Database and Data Structure
           </Title>
